e10b635752c5c0e38776ed24a7286bdefa7d2e12
Beck Boan <beckmboan@gmail.com>
2024-07-12 21:57:56 -0400

    Added GraphQL post CRUD and apollo-express middleware.

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,18 +1,24 @@
 import { Entity, PrimaryKey, Property, OptionalProps } from "@mikro-orm/core";
+import { ObjectType, Field, Int } from "type-graphql";
 
+@ObjectType()
 @Entity()
 export class Post {
   [OptionalProps]?: "createdAt" | "updatedAt";
 
+  @Field(() => Int)
   @PrimaryKey()
   id!: number;
 
+  @Field(() => String)
   @Property({ type: "string" })
   title!: string;
 
+  @Field(() => String)
   @Property({ type: "date", defaultRaw: "now()" })
   createdAt = new Date();
 
+  @Field(() => String)
   @Property({ type: "date", onUpdate: () => new Date(), defaultRaw: "now()" })
   updatedAt = new Date();
 }
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,58 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/postgresql";
 import mikroOrmConfig from "./mikro-orm.config";
-import { Post } from "./entities/Post";
+import express from "express";
+import { ApolloServer } from "@apollo/server";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import cors from "cors";
+import { expressMiddleware } from "@apollo/server/express4";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   try {
+    // -- ORM SETUP -- //
     // Initialize ORM and run migrations
     const orm = await MikroORM.init(mikroOrmConfig);
     await orm.getMigrator().up();
-
-    // Fork the EntityManager for specific operations
+    // Fork the EntityManager
     const em = orm.em.fork();
 
-    // Create a new Post entity
-    const post = em.create(Post, {
-      title: "my first post",
+    // -- SERVER SETUP -- //
+    const app = express();
+
+    const server = new ApolloServer({
+      schema: await buildSchema({
+        resolvers: [HelloResolver, PostResolver],
+        validate: false,
+      }),
     });
 
-    await em.persistAndFlush(post);
+    await server.start();
 
-    console.log(post);
+    app.use(
+      "/",
+      cors<cors.CorsRequest>(),
+      express.json(),
+      expressMiddleware(server, {
+        context: async () => ({ em }),
+      })
+    );
+
+    app.listen(4000, () => {
+      console.log("Server listening on port 4000");
+    });
+
+    // Create a new Post entity
+    // const post = em.create(Post, {
+    //   title: "my first post",
+    // });
+
+    // await em.persistAndFlush(post);
+
+    // const post = await em.find(Post, {});
+
+    // console.log(post);
   } catch (err) {
     console.error(err);
   }
